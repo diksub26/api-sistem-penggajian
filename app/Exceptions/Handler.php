82b102e250b50971f06a2e47b54f5101a4e6f1ac
diksub <diki@dev.netkromsolution.com>
2022-10-05 21:37:22 +0700

    core : add http 404 found exception

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,8 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -45,6 +47,17 @@ class Handler extends ExceptionHandler
     {
         $this->reportable(function (Throwable $e) {
             //
+        });
+
+        $this->renderable(function (NotFoundHttpException $e, Request $request)
+        {
+            if($request->wantsJson()) {
+                return response()->json([
+                    'message' => "Data tidak ditemukan.",
+                    'data' => null,
+                    'error' => null
+                ], 404);
+            }
         });
     }
 }
