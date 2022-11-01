604c3d6655918d51d9aeee0c64755f5a0564300a
diksub <diki@dev.netkromsolution.com>
2022-11-01 15:43:33 +0700

    update: type data filed 'value' in setting

--- database/migrations/2022_10_31_044459_create_settings_table.php
+++ database/migrations/2022_10_31_044459_create_settings_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('settings', function (Blueprint $table) {
             $table->id();
             $table->string('name', 100);
-            $table->integer('value');
+            $table->string('value');
             $table->timestamps();
         });
     }
