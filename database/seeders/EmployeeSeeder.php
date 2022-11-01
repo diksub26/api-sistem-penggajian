90310fd0b407ac4461a379cd862655f165114ce6
diksub <diki@dev.netkromsolution.com>
2022-11-01 15:35:46 +0700

    update : add item to employee seeder

--- database/seeders/EmployeeSeeder.php
+++ database/seeders/EmployeeSeeder.php
@@ -17,7 +17,7 @@ class EmployeeSeeder extends Seeder
     public function run()
     {
         $employe = Employee::create([
-            'no_induk' => '12097182',
+            'no_induk' => '11111110',
             'fullname' => 'Diki Subagja',
             'gender' => 'L',
             'place_of_birth' => 'Bandung',
@@ -34,7 +34,51 @@ class EmployeeSeeder extends Seeder
         $employe->user()->create([
             'employee_id' => $employe->id,
             'email' => 'dikisubagja50@gmail.com',
+            'role' => 'manajer',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        $admin = Employee::create([
+            'no_induk' => '11111111',
+            'fullname' => 'Admin',
+            'gender' => 'L',
+            'place_of_birth' => 'Bandung',
+            'dob' => '1997-12-14',
+            'address' => 'Jl. Test',
+            'religion' => 'ISLAM',
+            'no_hp' => '081111110223',
+            'employee_position_id' => '1',
+            'assignment_date' => '2019-08-01',
+            'division' => 'Project',
+            'functional_salary' => '500000',
+        ]);
+
+        $admin->user()->create([
+            'employee_id' => $employe->id,
+            'email' => 'admin@gmail.com',
             'role' => 'admin',
+            'password' => Hash::make('12345678'),
+        ]);
+
+        $karyawan = Employee::create([
+            'no_induk' => '11111111',
+            'fullname' => 'Karyawan 1',
+            'gender' => 'L',
+            'place_of_birth' => 'Bandung',
+            'dob' => '1997-12-14',
+            'address' => 'Jl. Test',
+            'religion' => 'ISLAM',
+            'no_hp' => '081111110223',
+            'employee_position_id' => '3',
+            'assignment_date' => '2019-08-01',
+            'division' => 'Project',
+            'functional_salary' => '500000',
+        ]);
+
+        $karyawan->user()->create([
+            'employee_id' => $employe->id,
+            'email' => 'karyawan@gmail.com',
+            'role' => 'karyawan',
             'password' => Hash::make('12345678'),
         ]);
     }
